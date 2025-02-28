ec76ad24e46525daedfaa1a863b50a2f1968f220
ggabrielfh <gabrielfaveroheller@gmail.com>
2025-02-28 16:35:12 -0300

    first commit

---
+++ costs/src/components/pages/Projects.js
@@ -0,0 +1,10 @@
+import Message from "../layout/Message"
+
+function Projects() {
+    return <div>
+        <h1>Meus Projetos</h1>
+        <Message msg="Alguma mensagem" type="error" />
+    </div>
+}
+
+export default Projects
